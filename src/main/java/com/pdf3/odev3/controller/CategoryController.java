8643b7198c1a95f8cd1dde1aeb63d697beffbf56
I-Merz-I <24010903102@subu.edu.tr>
2025-09-16 16:17:33 +0300

    Update CategoryController.java

--- src/main/java/com/pdf3/odev3/controller/CategoryController.java
+++ src/main/java/com/pdf3/odev3/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.pdf3.odev3.controller;
 import com.pdf3.odev3.model.Category;
 import com.pdf3.odev3.repository.CategoryRepository;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -14,9 +15,15 @@ public class CategoryController {
         this.repo = repo;
     }
 
+    // GET → tüm kategorileri listele
     @GetMapping
     public List<Category> list() {
         return repo.findAll();
     }
 
+    // POST → yeni kategori ekle
+    @PostMapping
+    public Category create(@RequestBody Category category) {
+        return repo.save(category);
+    }
 }
